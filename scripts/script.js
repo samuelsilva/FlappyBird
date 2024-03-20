0151d5dbd674b0c417d2c6ca0d1b584d0619813f
samuelsilva <samucasilva@gmail.com>
2024-03-20 17:49:11 -0300

    GameOver sound and GameOver Game

--- scripts/script.js
+++ scripts/script.js
@@ -18,12 +18,29 @@ var score_text = new Text();
 
 var fly = new Audio("assets/sounds/wing.ogg");
 var coin_pick = new Audio("assets/sounds/point.ogg");
+var game_over = new Audio("assets/sounds/hit.ogg");
 
+var play = true;
 
 document.addEventListener("click", function(event){
     bird.speed -= 15;
     fly.play();
 });
+
+function collision() {
+    if(bird.collide(coin)) {
+        if(coin.set_visible){
+            coin.set_visible = false;
+            score += 1;
+            coin_pick.play();
+        }
+    }
+ 
+    if(bird.collide(pipe1) || bird.collide(pipe2)) {
+        game_over.play();
+        play = false;
+    }
+}
 
 function draw() {
     bg.draw();
@@ -38,34 +55,27 @@ function draw() {
 }
 
 function update() {
-    bg.move(1, -500, 0);
-    bg2.move(1, 0, 500);
-
-    ground.move(2, -500, 0);
-    ground2.move(2, 0, 500);
+    if(play) {
+        bg.move(1, -500, 0);
+        bg2.move(1, 0, 500);
     
-    bird.move();
-    bird.animation(8, 4, "bird");
-    bird.limits();
-
-    pipe1.move(1, -100, 500, pipe2);
-
-    coin.move(pipe1);
-    coin.animation(5, 5, "");
-
-    score_text.text = score;
-
-    if(bird.collide(coin)) {
-        if(coin.set_visible){
-            coin.set_visible = false;
-            score += 1;
-            coin_pick.play();
-        }
+        ground.move(2, -500, 0);
+        ground2.move(2, 0, 500);
+        
+        bird.move();
+        bird.animation(8, 4, "bird");
+        bird.limits();
+    
+        pipe1.move(1, -100, 500, pipe2);
+    
+        coin.move(pipe1);
+        coin.animation(5, 5, "");
+    
+        score_text.text = score;
+        
+        collision();
     }
- 
-    if(bird.collide(pipe1)) {
-    //    coin.set_visible = false;
-    }
+
 }
 
 function main() {
